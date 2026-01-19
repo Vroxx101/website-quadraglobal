5baa6639695699b351205528e413c7e7cd61c0e1
Vroxx101 <alfathirfahri23@gmail.com>
2026-01-19 18:55:40 +0700

    update url

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://quadraglobal.id/sitemap.xml',
+        sitemap: 'https://quadraglobal.vercel.app/sitemap.xml',
     }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://quadraglobal.id'
+    const baseUrl = 'https://quadraglobal.vercel.app'
 
     return [
         {
